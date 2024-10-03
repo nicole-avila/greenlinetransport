abfdbe3d47fd4f6d52a1da3d160555dfe9bfa5f0
nicole-avila <114564720+nicole-avila@users.noreply.github.com>
2024-10-03 08:27:12 +0200

    footer comp. + resp. done

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 "use client";
+import "./Footer.css";
 import { useState, useEffect } from "react";
 import client from "@/lib/contentful";
 
@@ -25,8 +26,46 @@ function Footer() {
   if (!footerData) {
     return <div>Loading...</div>;
   }
+  console.log("footerData***", footerData);
 
-  return <div>{console.log(footerData)}</div>;
+  return (
+    <div className="footer">
+      <div className="footer_content">
+        <div className="footer_shortcut">
+          <h2>genvägar</h2>
+          <li className="footer_shortcut-li">våra tjänster</li>
+          <li className="footer_shortcut-li">fair transport</li>
+          <li className="footer_shortcut-li">om oss</li>
+          <li className="footer_shortcut-li">kontakt</li>
+        </div>
+
+        <div className="footer_find-us">
+          <div>
+            <h2>hitta oss</h2>
+            <p>{footerData.fields.streetAddress}</p>
+            <p>{footerData.fields.postalcode}</p>
+            <p>{footerData.fields.city}</p>
+          </div>
+          <br />
+          <div>
+            <h2>kontakt</h2>
+            <p>{footerData.fields.phoneNumber}</p>
+            <p>{footerData.fields.mobileNumber}</p>
+            <p>{footerData.fields.email}</p>
+          </div>
+        </div>
+
+        <div className="footer_media">
+          <h2>sociala medier</h2>
+          <div className="footer_icon">
+            <img src="/images/instagram.svg" alt="" />
+            <img src="/images/vector-3.svg" alt="" />
+          </div>
+        </div>
+        <div className="footer_svg-bg"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,35 @@
+"use client";
 import Footer from "./components/Footer/Footer";
+import { useState, useEffect } from "react";
+import client from "@/lib/contentful";
 
 export default function Home() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await client.getEntries({
+          content_type: "landingPage",
+        });
+
+        if (response.items.length > 0) {
+          setData(response.items[0]);
+        }
+      } catch (error) {
+        console.error("Error fetching data", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
+      {console.log("data***", data)}
       <h1> glt</h1>
       <Footer />
     </div>
