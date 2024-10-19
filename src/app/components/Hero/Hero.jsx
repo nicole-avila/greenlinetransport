453afab4e10e88f49c9cf789d42e2369394fd487
nicole-avila <114564720+nicole-avila@users.noreply.github.com>
2024-10-19 18:20:43 +0200

    building Services Page + resp.

--- src/app/components/Hero/Hero.jsx
+++ src/app/components/Hero/Hero.jsx
@@ -1,7 +1,23 @@
+"use client";
 import "./Hero.css";
+import Image from "next/image";
 
-function Hero() {
-  return <div>Hero</div>;
+function Hero({ heroImage, heroTitle }) {
+  return (
+    <div className="hero">
+      <p className="hero__title">{heroTitle}</p>
+      {heroImage && (
+        <Image
+          src={heroImage}
+          alt="Hero Image"
+          layout="responsive"
+          width={100}
+          height={100}
+          priority
+        />
+      )}
+    </div>
+  );
 }
 
 export default Hero;
---
+++ src/app/services/page.js
@@ -0,0 +1,33 @@
+"use client";
+import React from "react";
+import Hero from "../components/Hero/Hero";
+import useContentful from "@/lib/contentful";
+import OurServices from "../components/OurServices/OurServices";
+
+function Services() {
+  const { data, loading, error } = useContentful("service");
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error loading data: {error.message}</div>;
+  }
+
+  if (!data) {
+    return <div>No data found</div>;
+  }
+
+  return (
+    <div>
+      <Hero
+        heroImage={`https:${data.heroImage.fields.file.url}`}
+        heroTitle={data.heroTitle}
+      />
+      <OurServices data={data} />
+    </div>
+  );
+}
+
+export default Services;
