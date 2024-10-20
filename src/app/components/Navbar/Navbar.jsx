b7d973938cba44ded9c8b37674e69daf26435a38
Emma Ramstedt <71063351+emmaramstedt@users.noreply.github.com>
2024-10-20 17:49:50 +0200

    add hero to form page

--- src/app/components/Navbar/Navbar.jsx
+++ src/app/components/Navbar/Navbar.jsx
@@ -1,9 +1,9 @@
-"use client"; // This component handles client-side rendering
+'use client'; // This component handles client-side rendering
 
-import Image from "next/image";
-import "./Navbar.css";
-import { useState } from "react";
-import Link from "next/link";
+import Image from 'next/image';
+import './Navbar.css';
+import { useState } from 'react';
+import Link from 'next/link';
 
 function Navbar() {
   const [showLinks, setShowLinks] = useState(false);
@@ -13,31 +13,33 @@ function Navbar() {
   };
 
   return (
-    <div className="navbar">
+    <div className='navbar'>
       <div>
-        <Link href="#">
+        <Link href='#'>
           <Image
-            className="navbar_logo"
-            src="/images/whiteLogoXs.svg"
-            alt="Logo"
-            width="185"
-            height="80"
+            className='navbar_logo'
+            src='/images/whiteLogoXs.svg'
+            alt='Logo'
+            width='185'
+            height='80'
           />
         </Link>
       </div>
-      <div className={`navbar_links ${showLinks ? "active" : ""}`}>
-        <li className="navbar_link-li">våra tjänster</li>
-        <li className="navbar_link-li">fair transport</li>
-        <li className="navbar_link-li">om oss</li>
-        <li className="navbar_link-li">kontakt</li>
+      <div className={`navbar_links ${showLinks ? 'active' : ''}`}>
+        <li className='navbar_link-li'>våra tjänster</li>
+        <li className='navbar_link-li'>fair transport</li>
+        <li className='navbar_link-li'>om oss</li>
+        <li className='navbar_link-li'>
+          <Link href='/kontakt'>kontakt</Link>
+        </li>
       </div>
       <Image
-        className="navbar_hamburger"
-        src="/images/whiteHamburger.svg"
-        alt="Toggle navigation"
+        className='navbar_hamburger'
+        src='/images/whiteHamburger.svg'
+        alt='Toggle navigation'
         onClick={handleShowLinks}
-        width="30"
-        height="30"
+        width='30'
+        height='30'
       />
     </div>
   );
