e8919948aec349821777d22bfd1c052cb1d77fc3
nicole-avila <114564720+nicole-avila@users.noreply.github.com>
2024-10-23 22:49:19 +0200

    landing page + responsive

---
+++ src/app/components/LandingPage/FairTransport.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import "./LandingPage.css";
+import Image from "next/image";
+import Link from "next/link";
+
+function FairTransport({ data }) {
+  return (
+    <div class="content-wrapper">
+      <div class="content-image">
+        <Image
+          src={`https:${data.fairTransportImage.fields.file.url}`}
+          alt="Fair Transport Image"
+          width={250}
+          height={300}
+          class="image-style"
+        />
+      </div>
+      <div class="content-text">
+        <h2>{data.fairTransportTitle}</h2>
+        <p>{data.fairTransportText}</p>
+        <br />
+        <br />
+        <div className="content-our-mission">
+          <Image
+            src="/images/globe.svg"
+            alt="icon in svg file"
+            width={25}
+            height={25}
+            className="landing-icon"
+          />
+          <p>
+            <strong>Our Mission:</strong> {data.fairTransportShortText}
+          </p>
+        </div>
+        <div class="content-icon">
+          <Image
+            src="/images/link-line.svg"
+            alt="icon in svg file"
+            width={25}
+            height={25}
+            className="landing-icon"
+          />
+          <Link
+            href={data.fairTransportLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="content-link"
+          >
+            Länk till Fair Transport
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FairTransport;
