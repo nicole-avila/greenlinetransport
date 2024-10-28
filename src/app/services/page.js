67e883884895d78a7d389ce134e89841962651c4
Emma Ramstedt <71063351+emmaramstedt@users.noreply.github.com>
2024-10-28 10:14:49 +0100

    fix merge conflict

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -1,11 +1,11 @@
-"use client";
-import React from "react";
-import Hero from "../components/Hero/Hero";
-import OurServices from "../components/OurServices/OurServices";
-import useContentful from "@/lib/contentful";
+'use client';
+import React from 'react';
+import Hero from '../components/Hero/Hero';
+import useContentful from '@/lib/contentful';
+import ServiceCardLarge from '../components/ServiceCardLarge/ServiceCardLarge';
 
 function Services() {
-  const { data, loading, error } = useContentful("service");
+  const { data, loading, error } = useContentful('landingPage');
 
   if (loading) {
     return <div>Loading...</div>;
@@ -24,8 +24,31 @@ function Services() {
       <Hero
         heroImage={`https:${data.heroImage.fields.file.url}`}
         heroTitle={data.heroTitle}
+        heroParagraph={data.heroParagraph}
       />
-      <OurServices data={data} />
+      <div className='margins'>
+        <ServiceCardLarge
+          imageUrl={`https:${data.thermoDistribution.fields.image.fields.file.url}`}
+          imageAlt={data.thermoDistribution.fields.image.fields.title}
+          title={data.thermoDistribution.fields.title}
+          id='thermo-distribution'
+          longText={data.thermoDistribution.fields.longText}
+        />
+        <ServiceCardLarge
+          imageUrl={`https:${data.budservice.fields.image.fields.file.url}`}
+          imageAlt={data.budservice.fields.image.fields.title}
+          title={data.budservice.fields.title}
+          id='budservice'
+          longText={data.budservice.fields.longText}
+        />
+        <ServiceCardLarge
+          imageUrl={`https:${data.styckegodsOmlastning.fields.image.fields.file.url}`}
+          imageAlt={data.styckegodsOmlastning.fields.image.fields.title}
+          title={data.styckegodsOmlastning.fields.title}
+          id='styckegods-omlastning'
+          longText={data.styckegodsOmlastning.fields.longText}
+        />
+      </div>{' '}
     </div>
   );
 }
