4b9710a0f18be9dd982ca776dfe8a63969f7d14b
Emma Ramstedt <71063351+emmaramstedt@users.noreply.github.com>
2024-12-12 21:15:13 +0100

    staffcard image placeholder

--- src/app/components/StaffCards/StaffCards.jsx
+++ src/app/components/StaffCards/StaffCards.jsx
@@ -9,13 +9,22 @@ export default function StaffCards({ staff }) {
         staff.map((person, key) => {
           return (
             <div className='staff' key={key}>
-              <div>
-                <Image
-                  src={`https:${person.fields.image.fields.file.url}`}
-                  alt={person.fields.name}
-                  width={250}
-                  height={250}
-                />
+              <div className='staff-image'>
+                {person.fields.image ? (
+                  <Image
+                    src={`https:${person.fields.image.fields.file.url}`}
+                    alt={person.fields.name}
+                    width={250}
+                    height={250}
+                  />
+                ) : (
+                  <Image
+                    src='/images/profileplaceholder.svg'
+                    alt={person.fields.name}
+                    width={250}
+                    height={250}
+                  />
+                )}
               </div>
               <div className='staff-name'>{person.fields.name}</div>
               <div className='staff-title'>{person.fields.jobTitle}</div>
